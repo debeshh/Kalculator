5dabe36c186d8a095c831d0b98672ca7d0c6c7ca
Debesh Behera <anshubehera112@gmail.com>
2025-07-17 16:02:17 +0530

    updated script

--- script.js
+++ script.js
@@ -29,9 +29,9 @@ overlay.innerHTML = `
   <div class="card">
     <h2>made by Debesh.</h2>
     <div class="links">
-        <a href='https://github.com/debeshh/Kalculator'><img src="git.svg"/></a>
-        <a href='https://www.instagram.com/debeessh/'><img src="insta1.svg"/></a>
-        <a href='https://debeshbehera.netlify.app/'><img id="profile" src="profile-men.gif"/></a>
+        <a href='https://github.com/debeshh/Kalculator'><img src="assets/git.svg"/></a>
+        <a href='https://www.instagram.com/debeessh/'><img src="assets/insta1.svg"/></a>
+        <a href='https://debeshbehera.netlify.app/'><img id="profile" src="assets/profile-men.gif"/></a>
     </div>
     
     <button id="close-overlay">x</button>
